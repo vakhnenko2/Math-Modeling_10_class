48dafeafe19bea7afd7ddfed5dbd5f0007039e87
vakhnenko2 <56309115+vakhnenko2@users.noreply.github.com>
2019-10-22 17:49:38 +0200

    Im BEST

---
+++ Задание 2.py
@@ -0,0 +1,12 @@
+print("---------Задание 2---------")
+import modul_const as mc
+import numpy as np
+import math as mt
+
+v=mt.sqrt((mc.g)*(mc.h)*((mt.tan(mc.B))**2))/(2*((mt.cos(mc.a))**2)*(1-(mt.tan(mc.B))*(mt.tan(mc.a))))
+print(v)
+
+N=(2/(mt.sqrt(mc.pi)))*((mc.h)/(((mc.k)*(mc.T))**1.5))*(mc.e)**(-(mc.э)/((mc.k)*(mc.T)))*((mc.э)**((mc.T)/2))
+print(N)
+
+print("---------Задание 3---------")
